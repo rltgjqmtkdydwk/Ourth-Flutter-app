ddc7bd8be47d8658c45d0b8a40e23f5497999ebc
EunchongKim <slvrcods22@gmail.com>
2023-04-01 00:55:20 +0900

    fix: platform error

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:ourth_flutter_app_webview/webview.dart';
-// import 'intro.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const WebViewApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+const webUrl = 'https://ourth-frontend.vercel.app/';
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (request) {
+          if (request.url.startsWith(webUrl)) {
+            return NavigationDecision.navigate;
+          } else {
+            _launchURL(request.url as Uri);
+            return NavigationDecision.prevent;
+          }
+        },
+      ))
+      ..loadRequest(
+        Uri.parse(webUrl),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Ourth',
-      theme: ThemeData(
-        primarySwatch: Colors.lightGreen
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(
+          controller: controller,
         ),
-      home: SplashScreen(),
+      ),
     );
   }
+
+  _launchURL(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:ourth_flutter_app_webview/webview.dart';
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Ourth',
+//       theme: ThemeData(
+//         primarySwatch: Colors.lightGreen
+//         ),
+//       home: WebViewApp(),
+//     );
+//   }
+// }
--- lib/webview.dart
+++ lib/webview.dart
@@ -1,35 +1,95 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:flutter/material.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
+// class WebViewApp extends StatefulWidget {
+//   const WebViewApp({super.key});
 
-  @override
-  State<WebViewApp> createState() => _WebViewAppState();
-}
+//   @override
+//   State<WebViewApp> createState() => _WebViewAppState();
+// }
 
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+// class _WebViewAppState extends State<WebViewApp> {
+//   late final WebViewController controller;
 
-  @override
-  void initState() {
-    super.initState();
-    controller = WebViewController()
-      ..loadRequest(
-        Uri.parse('https://ourth-frontend.vercel.app/'),
-      );
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller = WebViewController()
+//       ..loadRequest(
+//         Uri.parse('https://ourth-frontend.vercel.app/'),
+//       );
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ourth'),
-        automaticallyImplyLeading: false,
-      ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Ourth'),
+//         automaticallyImplyLeading: false,
+//       ),
+//       body: WebViewWidget(
+//         controller: controller,
+//       ),
+//     );
+//   }
+// }
+
+
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:webview_flutter_android/webview_flutter_android.dart';
+// import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+
+// class OurthViewScreen extends StatefulWidget {
+//   @override
+//   State<OurthViewScreen> createState() => _OurthViewScreenState();
+// }
+
+// class _OurthViewScreenState extends State<OurthViewScreen> {
+
+//   late final WebViewController webViewController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     webViewController = WebViewController()
+//     ..setJavaScriptMode(JavaScriptMode.unrestricted)
+//     ..setBackgroundColor(const Color(0x00000000))
+//     ..setNavigationDelegate(
+//     NavigationDelegate(
+//       onProgress: (int progress) {
+//         //CircularProgressIndicator();
+//       },
+//       onPageStarted: (url) {
+
+//       },
+//       onPageFinished: (url) {
+
+//       },
+//       onWebResourceError: (WebResourceError error) {
+        
+//       },
+//       onNavigationRequest: (NavigationRequest request) {
+//         if (request.url.startsWith('https://ourth-frontend.vercel.app/')) {
+//           return NavigationDecision.prevent;
+//         }
+//         return NavigationDecision.navigate;
+//       },
+//     ),
+//   )
+//   ..loadRequest(Uri.parse('https://ourth-frontend.vercel.app/'));
+//   }
+  
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return Scaffold(
+//       body: WebViewWidget(
+//         controller: webViewController,
+//       ),
+//     );
+//   }
+// }
